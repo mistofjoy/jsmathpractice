e8b131cfedddcd9b289224f1cd43ab3d23941e74
Jhonkar Sufia <windyforest13@gmail.com>
2021-09-06 14:15:13 -0400

    mode, media, and median fully functional

--- mode/mode.js
+++ mode/mode.js
@@ -50,7 +50,7 @@ data1.addEventListener("input", () => {
 data2.addEventListener("input", () => {
     if (data2.value != '') {
         label3.style.display = "flex";
-        data[1] = parseFloat(data1.value);
+        data[1] = parseFloat(data2.value);
     } 
     else {
         data[1] = 0;
@@ -60,7 +60,7 @@ data2.addEventListener("input", () => {
 data3.addEventListener("input", () => {
     if (data3.value != '') {
         label4.style.display = "flex";
-        data[2] = parseFloat(data1.value);
+        data[2] = parseFloat(data3.value);
     } 
     else {
         data[2] = 0;
@@ -70,7 +70,7 @@ data3.addEventListener("input", () => {
 data4.addEventListener("input", () => {
     if (data4.value != '') {
         label5.style.display = "flex";
-        data[3] = parseFloat(data1.value);
+        data[3] = parseFloat(data4.value);
     } 
     else {
         data[3] = 0;
@@ -80,7 +80,7 @@ data4.addEventListener("input", () => {
 data5.addEventListener("input", () => {
     if (data5.value != '') {
         label6.style.display = "flex";
-        data[4] = parseFloat(data1.value);
+        data[4] = parseFloat(data5.value);
     } 
     else {
         data[4] = 0;
@@ -90,7 +90,7 @@ data5.addEventListener("input", () => {
 data6.addEventListener("input", () => {
     if (data6.value != '') {
         label7.style.display = "flex";
-        data[5] = parseFloat(data1.value);
+        data[5] = parseFloat(data6.value);
     } 
     else {
         data[5] = 0;
@@ -100,7 +100,7 @@ data6.addEventListener("input", () => {
 data7.addEventListener("input", () => {
     if (data7.value != '') {
         label8.style.display = "flex";
-        data[6] = parseFloat(data1.value);
+        data[6] = parseFloat(data7.value);
     } 
     else {
         data[6] = 0;
@@ -110,7 +110,7 @@ data7.addEventListener("input", () => {
 data8.addEventListener("input", () => {
     if (data8.value != '') {
         label9.style.display = "flex";
-        data[7] = parseFloat(data1.value);
+        data[7] = parseFloat(data8.value);
     } 
     else {
         data[7] = 0;
@@ -120,7 +120,7 @@ data8.addEventListener("input", () => {
 data9.addEventListener("input", () => {
     if (data9.value != '') {
         label10.style.display = "flex";
-        data[8] = parseFloat(data1.value);
+        data[8] = parseFloat(data9.value);
     } 
     else {
         data[8] = 0;
@@ -130,7 +130,7 @@ data9.addEventListener("input", () => {
 data10.addEventListener("input", () => {
     if (data10.value != '') {
         label11.style.display = "flex";
-        data[9] = parseFloat(data1.value);
+        data[9] = parseFloat(data10.value);
     } 
     else {
         data[9] = 0;
@@ -140,7 +140,7 @@ data10.addEventListener("input", () => {
 data11.addEventListener("input", () => {
     if (data11.value != '') {
         label12.style.display = "flex";
-        data[10] = parseFloat(data1.value);
+        data[10] = parseFloat(data11.value);
     } 
     else {
         data[10] = 0;
@@ -150,7 +150,7 @@ data11.addEventListener("input", () => {
 data12.addEventListener("input", () => {
     if (data12.value != '') {
         label13.style.display = "flex";
-        data[11] = parseFloat(data1.value);
+        data[11] = parseFloat(data12.value);
     } 
     else {
         data[11] = 0;
@@ -160,7 +160,7 @@ data12.addEventListener("input", () => {
 data13.addEventListener("input", () => {
     if (data13.value != '') {
         label14.style.display = "flex";
-        data[12] = parseFloat(data1.value);
+        data[12] = parseFloat(data13.value);
     } 
     else {
         data[12] = 0;
@@ -170,7 +170,7 @@ data13.addEventListener("input", () => {
 data14.addEventListener("input", () => {
     if (data14.value != '') {
         label15.style.display = "flex";
-        data[13] = parseFloat(data1.value);
+        data[13] = parseFloat(data14.value);
     } 
     else {
         data[13] = 0;
@@ -179,11 +179,60 @@ data14.addEventListener("input", () => {
 
 data15.addEventListener("input", () => {
     if (data15.value != '') {
-        data[14] = parseFloat(data1.value);
+        data[14] = parseFloat(data15.value);
     } 
     else {
         data[14] = 0;
     }
 })
 
-//Media aritmetica
+//calculos
+let button = document.getElementById("calculate");
+let mediaResult = document.getElementById("media");
+let medianResult = document.getElementById("median");
+let modeResult = document.getElementById("mode");
+
+const media = () => {
+    let mediaCount = 0;
+    for (let i of data) {
+        mediaCount += i;
+    }
+    let mediaFinal = mediaCount / data.length;
+    mediaResult.innerHTML = `${mediaFinal}`;
+}
+
+const median = () => {
+    let i = Math.ceil(data.length / 2) - 1;
+    let medianFinal;
+    if (data.length % 2 == 0) {
+        medianFinal = (data[i] + data[i + 1]) / 2;
+    }
+    else {
+        medianFinal = data[i];
+    }
+    medianResult.innerHTML = `${medianFinal}`;
+}
+
+const mode = () => {
+    let object = {};
+    data.map((element) => {
+        if (object[element]) {
+            object[element] += 1;
+        }
+        else {
+            object[element] = 1;
+        }
+    })
+    const array = Object.entries(object).sort((entryA, entryB) => {
+        return entryA[1] - entryB[1];
+    });
+    let modeInter = array[array.length - 1];
+    let modeFinal = modeInter[0];
+    modeResult.innerHTML = modeFinal;
+}
+
+button.addEventListener("click", () => {
+    media();
+    median();
+    mode();
+})
